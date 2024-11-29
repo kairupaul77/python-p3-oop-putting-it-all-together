34e1cf0c3f3a1517821c9913c7abe9bab6984435
paul kairu <paulkairukamau@gmail.com>
2024-11-30 02:27:24 +0300

    lib

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(this, title, page_count):
+        this.title = title
+        this._page_count = None  # Using a private attribute to enforce validation
+        this.page_count = page_count  # This will call the setter method
+
+    @property
+    def page_count(this):
+        return this._page_count
+
+    @page_count.setter
+    def page_count(this, value):
+        if isinstance(value, int):
+            this._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(this):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(this, brand, size):
+        this.brand = brand
+        this._size = None  # Private attribute for size validation
+        this.size = size   # This will call the setter method
+        this.condition = None  # Initially unset
+
+    @property
+    def size(this):
+        return this._size
+
+    @size.setter
+    def size(this, value):
+        if isinstance(value, int):
+            this._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(this):
+        this.condition = "New"  
+        print("Your shoe is as good as new!")
